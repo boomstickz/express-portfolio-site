7fe50f7e705d998affa6b08e1bedb377c24e8b48
boomstickz <ali.alam@ontariotechu.net>
2025-11-07 20:06:26 -0500

    Added a working navigation bar which includes about, project and contact pages, along with css

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+// this is the main express app file. it creates the app instance,
+// wires the view engine (ejs), serves /public, and mounts routes.
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');   // our page routes
+var usersRouter = require('./routes/users');   // default sample route
 
-var app = express();
+var app = express(); // <-- this line creates the app. without it, app.use blows up
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -27,15 +30,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler (kept simple)
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { message: err.message, error: err });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* one route per page. express calls res.render(viewname) to send an ejs file. */
+
+/* home */
+router.get('/', (req, res) => {
+  res.render('home');      // looks for views/home.ejs
+});
+
+/* about */
+router.get('/about', (req, res) => {
+  console.log('hit /about');
+  res.render('about');     // looks for views/about.ejs
+});
+
+/* projects */
+router.get('/projects', (req, res) => {
+  res.render('projects');  // views/projects.ejs
+});
+
+/* contact */
+router.get('/contact', (req, res) => {
+  res.render('contact');   // views/contact.ejs
 });
 
 module.exports = router;
